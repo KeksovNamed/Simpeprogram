1dec14d59da01b55ed55ce3d01aed271a68fddf7
KeksovNamed <157127979+KeksovNamed@users.noreply.github.com>
2024-01-21 10:45:07 +0300

    Update task.c

--- task.c
+++ task.c
@@ -6,9 +6,30 @@
 int main()
 {
 	int wholepart,fractional;
-	printf("Введите целую часть дробную часть(например 5 200)\n");
-	scanf("%d %d",&wholepart,&fractional);
-	printf("Вы ввели %d %d\n",wholepart,fractional);
+	double number;
+	printf("Введите число\n");
+	scanf("%Lf",&number);//теперь тут 5.0032
+	printf("Вы ввели %Lf\n",number);
+	int integer=number;//теперь тут 5
+	double remaindernum=number-integer;//теперь тут 0.0032
+	int ten=10;
+	int zero=0;
+	int numberstozero=0;
+	for(; remaindernum/1<10;)
+	{
+		remaindernum=remaindernum*ten;
+		numberstozero++;
+	}
+	double temprem=remaindernum;
+	temprem=temprem/0;
+	if(temprem=!0)
+	{
+		remaindernum=remaindernum*ten;
+		numberstozero++;
+	}
+	printf("Остаток целое =%g\n",remaindernum);
+	wholepart=integer;
+	fractional=remaindernum;
 	int length(int n)   //код взят из гугла
 	{
 		int l = 1;
@@ -16,9 +37,8 @@ int main()
 		return l;
 	}
 	int lengthnumbers = length(fractional);//дальше мой
-	printf("Длина дробной части =%d\n",lengthnumbers);
+	printf("Длина дробной части =%d\n",numberstozero);
 	int result=10;
-	int ten=10;
 	int flag=0;
 	for (int i = 0; i < lengthnumbers; i++)
 	{
@@ -38,8 +58,8 @@ int main()
 	}
 	result=result+fractional;
 	printf("Числитель готов и он равен %d\n",result);
-	double biglengthnumbers=lengthnumbers;
-	double bigdenominator=pow(10,lengthnumbers);
+	double biglengthnumbers=numberstozero;
+	double bigdenominator=pow(10,numberstozero);
 	int denominator=bigdenominator;
 	printf("Знаменатель готов и он равен %d\n",denominator);
 	printf("Дробь равна %d/%d\n",result,denominator);
