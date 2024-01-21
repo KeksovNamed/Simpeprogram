a6bfad25836e3c7538eac3ebfad576515213025d
KeksovNamed <157127979+KeksovNamed@users.noreply.github.com>
2024-01-21 07:43:49 +0300

    Add files via upload
    Converts a number to a fraction! Doesn't work with 0, enter without zeros

---
+++ task.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<math.h>
+
+int main()
+{
+	int wholepart,fractional;
+	printf("Введите целую часть дробную часть(например 5 200)\n");
+	scanf("%d %d",&wholepart,&fractional);
+	printf("Вы ввели %d %d\n",wholepart,fractional);
+	int length(int n)   //код взят из гугла
+	{
+		int l = 1;
+		for (; n = n /10; ++l);
+		return l;
+	}
+	int lengthnumbers = length(fractional);
+	printf("Длина дробной части =%d\n",lengthnumbers);
+	int result=10;
+	int ten=10;
+	int flag=0;
+	for (int i = 0; i < lengthnumbers; i++)
+	{
+
+		result = ten*wholepart;
+		if(flag<2)
+		{
+			flag=1;
+		}
+
+		if(flag<lengthnumbers)
+		{
+			ten=ten*10;
+			++flag;
+		}
+
+	}
+	result=result+fractional;
+	printf("Числитель готов и он равен %d\n",result);
+	double biglengthnumbers=lengthnumbers;
+	double bigdenominator=pow(10,lengthnumbers);
+	int denominator=bigdenominator;
+	printf("Знаменатель готов и он равен %d\n",denominator);
+	printf("Дробь равна %d/%d\n",result,denominator);
+	int gcd(int a, int b)	//алгоритм Евклида
+	{
+		int c;
+		while (b)
+		{
+			c = a % b;
+			a = b;
+			b = c;
+		}
+
+		return a;
+	}
+	int i = gcd(result,denominator);	//алгоритм Евклида,находим общее число для деления.
+	printf("Общее делимое число=%d\n", i);	//чему равно число для делания числителя и знаменателя.
+	if(i>0)
+	{
+		result=result/i;	//делим числитель на общее число для деления
+		denominator = denominator / i;	//делим знаменатель на общее число для деления
+	}
+	printf("Сокращаем дроби\n");
+	printf("%d/%d\n",result,denominator);
+	if(result>denominator);
+	{
+		printf("Приводим неправильную дробь в смешанное число\n");
+		int incomplete=result/denominator;
+		int remainder=result%denominator;
+		printf("Получаем %d %d/%d",incomplete,remainder,denominator);
+	}
+	return 0;
+}
