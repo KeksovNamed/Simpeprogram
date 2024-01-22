99c71e5cb51e19a6d368e12de0da84451856ff48
KeksovNamed <157127979+KeksovNamed@users.noreply.github.com>
2024-01-22 15:22:59 +0300

    Update task.c

--- task.c
+++ task.c
@@ -4,8 +4,7 @@
 #include <conio.h>
 #include<math.h>
 
-int main()
-{
+int main() {
 	int wholepart,fractional,number;
 	int fraction;
 	printf("Введите число.дробную часть\n");
@@ -15,8 +14,7 @@ int main()
 	printf("Остаток целое =%d\n",fraction);
 	wholepart=number;
 	fractional=fraction;
-	int length(int n)   //код взят из гугла
-	{
+	int length(int n) { //код взят из гугла
 		int l = 1;
 		for (; n = n /10; ++l);
 		return l;
@@ -27,17 +25,14 @@ int main()
 	int result=10;
 	int flag=0;
 	int ten=10;
-	for (int i = 0; i < lengthnumbers; i++)
-	{
+	for (int i = 0; i < lengthnumbers; i++) {
 
 		result = ten*wholepart;
-		if(flag<2)
-		{
+		if(flag<2) {
 			flag=1;
 		}
 
-		if(flag<lengthnumbers)
-		{
+		if(flag<lengthnumbers) {
 			ten=ten*10;
 			++flag;
 		}
@@ -51,11 +46,9 @@ int main()
 	printf("Введите количество незначащих нулей перед число(0) если таких нету\n");
 	int leadingzeros=0;
 	scanf("%d",&leadingzeros);
-	if(leadingzeros==0)
-	{
+	if(leadingzeros==0) {
 	}
-	if(leadingzeros>=1)
-	{
+	if(leadingzeros>=1) {
 		double bigleadingzero=leadingzeros;
 		double bigtemplead=pow(10,bigleadingzero);
 		leadingzeros=bigtemplead;
@@ -63,11 +56,9 @@ int main()
 	}
 	printf("Знаменатель готов и он равен %d\n",denominator);
 	printf("Дробь равна %d/%d\n",result,denominator);
-	int gcd(int a, int b)	//алгоритм Евклида
-	{
+	int gcd(int a, int b) {	//алгоритм Евклида
 		int c;
-		while (b)
-		{
+		while (b) {
 			c = a % b;
 			a = b;
 			b = c;
@@ -77,8 +68,7 @@ int main()
 	}
 	int i = gcd(result,denominator);	//алгоритм Евклида,находим общее число для деления.
 	printf("Общее делимое число=%d\n", i);	//чему равно число для делания числителя и знаменателя.
-	if(i>0)
-	{
+	if(i>0) {
 		result=result/i;	//делим числитель на общее число для деления
 		denominator = denominator / i;	//делим знаменатель на общее число для деления
 	}
@@ -89,7 +79,12 @@ int main()
 		printf("Приводим неправильную дробь в смешанное число\n");
 		int incomplete=result/denominator;
 		int remainder=result%denominator;
-		printf("Получаем %d %d/%d",incomplete,remainder,denominator);
+		if(incomplete==0) {
+			printf("Получаем %d/%d",remainder,denominator);
+		}
+		if(incomplete>=1) {
+			printf("Получаем %d %d/%d",incomplete,remainder,denominator);
+		}
 	}
 	return 0;
 }
